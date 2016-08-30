76e8a5857ea5d41a86e2e353200fa593b2274c8c
jamesjwood <jamesjwood@msn.com>
2016-08-30 19:56:02 -0400

    Use default user folder to get google files

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 : "${WORKSPACE:?There must be a WORKSPACE environment variable set}"
-: "${FIREBASE_SYMBOL_SERVICE_JSON:?There must be a FIREBASE_SYMBOL_SERVICE_JSON environment variable set}"
-: "${FIREBASE_ANALYTICS_PLIST:?There must be a FIREBASE_ANALYTICS_PLIST environment variable set}"
 
 pod install --project-directory=ClassfitteriOS/
+
+if [[ NODE_ENV == "production" ]]; then
+    FIREBASE_SYMBOL_SERVICE_JSON=~/FirebaseCrash-Live.json
+    FIREBASE_ANALYTICS_PLIST=~/GoogleService-Info-Live.plist
+else
+    FIREBASE_SYMBOL_SERVICE_JSON=~/FirebaseCrash-Development.json
+    FIREBASE_ANALYTICS_PLIST=~/GoogleService-Info-Development.plist
+fi
 
 #FIREBASE CRASH
 rm -rf ${FIREBASE_SERVICE_FILE}
