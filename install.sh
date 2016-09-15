7ef18f9c737f23477ff65686ac0bbf843c84f0b9
jamesjwood <jamesjwood@msn.com>
2016-09-15 13:53:46 -0400

    Fix install script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-: "${WORKSPACE:?There must be a WORKSPACE environment variable set}"
-#git clean -d -x -f
-
-export GEM_HOME=$HOME/.gem
 #!/bin/sh -xe
+
+: "${WORKSPACE:?There must be a WORKSPACE environment variable set}"
+git clean -d -x -f
+export GEM_HOME=$HOME/.gem
+
 
 export PATH=$GEM_HOME/bin:$PATH
 . $(brew --prefix nvm)/nvm.sh
@@ -16,11 +17,11 @@ gem install bundler
 bundle install
 pod repo update
 pod install --project-directory=ClassfitteriOS/
-echo $NODE_ENV
+
 
 if [[ $LOCATION == "CI" ]]; then
     npm install cftool
-else
+fi
 
 FIREBASE_SYMBOL_SERVICE_JSON=${HOME}/FirebaseCrash-Development.json
 FIREBASE_ANALYTICS_PLIST=${HOME}/GoogleService-Info-Development.plist
@@ -28,7 +29,9 @@ FIREBASE_SERVICE_FILE=${WORKSPACE}/ClassfitteriOS/FirebaseServiceAccount.json
 FIREBASE_ANALYTICS_FILE=${WORKSPACE}/ClassfitteriOS/GoogleService-Info.plist
 #FIREBASE CRASH
 rm -rf FIREBASE_SERVICE_FILE
+echo "cp ${FIREBASE_SYMBOL_SERVICE_JSON} ${FIREBASE_SERVICE_FILE}"
 cp $FIREBASE_SYMBOL_SERVICE_JSON $FIREBASE_SERVICE_FILE
 #FIREBASE ANALYTICS
-rm -rf FIREBASE_ANALYTICS_FILE
+echo "rm -rf ${FIREBASE_ANALYTICS_FILE}"
+rm -rf $FIREBASE_ANALYTICS_FILE
 cp $FIREBASE_ANALYTICS_PLIST $FIREBASE_ANALYTICS_FILE
