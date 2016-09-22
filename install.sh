8cec6e43e1de83f11255a08c9420b2ee0a4f41f0
jamesjwood <james.wood@datatransparency.com>
2016-09-22 17:40:25 -0400

    Set path on CI

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -xe
 
+if [[ $LOCATION == "CI" ]]; then
+    export NODE_ENV=production
+    export PATH=/Users/buildservice/.rvm/gems/ruby-2.3.0/bin:/Users/buildservice/.rvm/gems/ruby-2.3.0@global/bin:/Users/buildservice/.rvm/rubies/ruby-2.3.0/bin:/Users/buildservice/.rvm/bin:/Users/buildservice/.nvm/versions/node/v6.5.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/go/bin
+fi
+
 brew install gcovr
 brew install tailor
 nvm install
