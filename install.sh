3e9ca17ab5377ed4730822cbbd989dcb8967176a
jamesjwood <jamesjwood@msn.com>
2016-09-24 16:09:35 -0400

    Add nvm to shell

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ if [[ $LOCATION == "CI" ]]; then
     export PATH=/Users/buildservice/.rvm/gems/ruby-2.3.0/bin:/Users/buildservice/.rvm/gems/ruby-2.3.0@global/bin:/Users/buildservice/.rvm/rubies/ruby-2.3.0/bin:/Users/buildservice/.rvm/bin:/Users/buildservice/.nvm/versions/node/v6.5.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/go/bin
 fi
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
 brew install gcovr
 brew install tailor
 nvm install
