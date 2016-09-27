fd4fa924020e9b6a55c0820e7cd6e37dbc5d02ee
jamesjwood <jamesjwood@msn.com>
2016-09-27 11:29:26 -0400

    Fix build

--- scripts/test-ui.sh
+++ scripts/test-ui.sh
@@ -13,14 +13,12 @@ TEST_REPORTS_FOLDER="${BIN_DIRECTORY}/reports"
 
 defaults write com.apple.iphonesimulator ConnectHardwareKeyboard 0
 if [[ ${LOCATION} == 'CI' ]]; then
-    DESTINATION="'platform=iOS Simulator,name=iPhone SE,OS=10.0'"
+    DESTINATION="platform=iOS Simulator,name=iPhone SE,OS=10.0"
 else
-    DESTINATION="'platform=iOS Simulator,name=iPhone 6,OS=10.0'"
+    DESTINATION="platform=iOS Simulator,name=iPhone 6,OS=10.0"
 fi
 
-echo "/usr/bin/xcodebuild test -scheme ${UI_TEST_SCHEME} -derivedDataPath ${BIN_DIRECTORY} -workspace ${XCODE_WORKSPACE_FILE} -configuration Debug -destination $DESTINATION GOOGLE_APP_ID=${GOOGLE_APP_ID} -enableCodeCoverage YES | ocunit2junit"
-/usr/bin/xcodebuild test -scheme ${UI_TEST_SCHEME} -derivedDataPath ${BIN_DIRECTORY} -workspace ${XCODE_WORKSPACE_FILE} -configuration Debug -destination $DESTINATION GOOGLE_APP_ID=${GOOGLE_APP_ID} -enableCodeCoverage YES | ocunit2junit
-
+/usr/bin/xcodebuild test -scheme ${UI_TEST_SCHEME} -derivedDataPath ${BIN_DIRECTORY} -workspace ${XCODE_WORKSPACE_FILE} -configuration Debug -destination "${DESTINATION}" GOOGLE_APP_ID=${GOOGLE_APP_ID} -enableCodeCoverage YES | ocunit2junit
 
 mv test-reports $TEST_REPORTS_FOLDER/
 rm -rf ${JOB_STATUS_FILE}
