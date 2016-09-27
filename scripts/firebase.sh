6504b687b2175427fa76bcdaf6eb20fcf6fedd4d
jamesjwood <jamesjwood@msn.com>
2016-09-27 10:12:31 -0400

    Fix firebase

--- scripts/firebase.sh
+++ scripts/firebase.sh
@@ -2,11 +2,9 @@
 
 source scripts/environment-variables.sh
 
-: "${FIREBASE_SOURCE_DIRECTORY:?There must be a FIREBASE_SOURCE_DIRECTORY environment variable set}"
-: "${WORKSPACE:?There must be a WORKSPACE environment variable set}"
-: "${XCODE_WORKSPACE_DIRECTORY_NAME:?There must be a XCODE_WORKSPACE_DIRECTORY_NAME environment variable set}"
+: "${FIREBASE_DIRECTORY_NAME:?There must be a FIREBASE_DIRECTORY_NAME environment variable set}"
 
 PROJECT_DIR=ClassfitteriOS
 rm -Rf ~/${FIREBASE_DIRECTORY_NAME}/
 mkdir ~/${FIREBASE_DIRECTORY_NAME}
-scp buildservice@192.168.5.25:${FIREBASE_DIRECTORY_NAME}/*.* ${FIREBASE_DIRECTORY_NAME}/
+scp buildservice@192.168.5.25:${FIREBASE_DIRECTORY_NAME}/*.* ~/${FIREBASE_DIRECTORY_NAME}/
